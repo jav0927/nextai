eab0274b8586c95054ce73f45e5f4e951cb2762b
jav0927 <joseadolfovillalobos@email.com>
2020-09-20 23:19:55 +0000

    worked on auto_augment

--- nextai_lib/_nbdev.py
+++ nextai_lib/_nbdev.py
@@ -16,7 +16,14 @@ index = {"device": "03_auto_augment.ipynb",
          "get_anchors": "01_anchor_boxes.ipynb",
          "show_anchors": "01_anchor_boxes.ipynb",
          "pad_output": "02_inference.ipynb",
-         "get_activ_offsets_mns": "02_inference.ipynb"}
+         "get_activ_offsets_mns": "02_inference.ipynb",
+         "fastai2pil_basis": "03_auto_augment.ipynb",
+         "pil2fastai_basis": "03_auto_augment.ipynb",
+         "flip_horizontal": "03_auto_augment.ipynb",
+         "swap_xy_coords": "03_auto_augment.ipynb",
+         "rotate_bb": "03_auto_augment.ipynb",
+         "shear_x_bboxes": "03_auto_augment.ipynb",
+         "rotate_bboxes": "03_auto_augment.ipynb"}
 
 modules = ["vision_core.py",
            "anchor_boxes.py",
